7138f4a6a0a24d0308a8935b3d84e6b86322caea
Jake <jakeadmire1@gmail.com>
2019-02-14 15:57:06 -0700

    Squash merge bugs

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ export default function Footer(props) {
     return (
         <div className='footer'>
             <SearchBar />
-            <ConsoleBar {...props}/>
+            <PlatformBar {...props}/>
             <GenreBar {...props} />
             <MultiBar />
             <LuckyBar />
