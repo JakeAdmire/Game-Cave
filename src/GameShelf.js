225ec60660dea8501bb44d7602c420d3ef3ea6e0
SiimonStark <justinpyktel@gmail.com>
2019-02-16 23:21:11 -0700

    starting popup, need to pull from master

--- src/GameShelf.js
+++ src/GameShelf.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import GameCard from './GameCard';
+import Popup from './Popup';
 import './GameShelf.css';
 
 export default function Gameshelf(props) {
   return(
     <div className= 'game-shelf'>
+      <section className="popup-container">
+        {// <Popup {...props}/>
+        }
+      </section>
       <article className='card-container'>
       {
         props.games.map((game, index) => {
@@ -13,7 +18,7 @@ export default function Gameshelf(props) {
           )
         })
       }
-      </article> 
+      </article>
     </div>
   )
 }
---
+++ src/Popup.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// export default props => 
+//       <div className="popup-overlay is-active">
+//         <div className="popup">
+//           <section className="img">
+//             <h2>Test</h2>
+//           </section>
+//           <section className="text-container">
+//             <h4>{this.props.games.title}</h4>
+//           </section>
+//         </div>
+//       </div>
