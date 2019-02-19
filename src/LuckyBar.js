be2b6f97b40c9a9125f3756a8a78d832c2f01983
Jake <jakeadmire1@gmail.com>
2019-02-19 16:19:29 -0700

    Add selector functionality to 'random' button

--- src/LuckyBar.js
+++ src/LuckyBar.js
@@ -3,15 +3,22 @@ import React, {Component} from 'react';
 export default class LuckyBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { active: false }
   }
   handleLucky = () => {
     this.props.updateState({isLucky: this.props.isLucky ? false : true});
+    this.toggleActive();
   }
-render() {
-  return(
-    <div className="lucky-bar search-section">
-      <button className="button lucky random-all" onClick={this.handleLucky}>RANDOM</button>
-    </div>
-  )}
+  toggleActive = () => {
+    console.log(this.state.active);
+    this.setState({ active: this.state.active ? false : true })
+  }
+  render() {
+    let buttonSelector = this.state.active ? "button lucky current" : "button lucky";
+    return(
+      <div className="lucky-bar search-section">
+        <button className={buttonSelector} onClick={this.handleLucky}>RANDOM</button>
+      </div>
+    )
+  }
 }
