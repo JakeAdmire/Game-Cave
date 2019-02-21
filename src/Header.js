ae34626559b44108b6aea8f7dc6deecc50d8238c
Jake <jakeadmire1@gmail.com>
2019-02-20 20:29:00 -0700

    Implement 'Clear Search' functionality and refactor LuckyBar.js

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Header () {
+export default function Header (props) {
 	return (
 		<div className="header">
 			<h1 className="title">GameCave</h1>
       <p>View Library</p>
-      <p>Clear Search</p>
+      <p onClick={props.resetState}>Clear Search</p>
 		</div>
 	);
 }
--- src/LuckyBar.js
+++ src/LuckyBar.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
 
 export default class LuckyBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { active: false }
-  }
   handleLucky = () => {
     this.props.updateState({isLucky: this.props.isLucky ? false : true});
-    this.setState({ active: this.state.active ? false : true })
   }
   render() {
-    let buttonSelector = this.state.active ? "button lucky current" : "button lucky";
+    let buttonSelector = this.props.isLucky ? "button lucky current" : "button lucky";
     return(
       <div className="lucky-bar search-section">
         <button className={buttonSelector} onClick={this.handleLucky}>RANDOM</button>
