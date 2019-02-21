867daddf1d8be70506082fbea7aa38eb577638f5
Jake <jakeadmire1@gmail.com>
2019-02-20 19:49:02 -0700

    Add 'View Library' and 'Clear Search' buttons to header

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,8 @@ export default function Header () {
 	return (
 		<div className="header">
 			<h1 className="title">GameCave</h1>
+      <p>View Library</p>
+      <p>Clear Search</p>
 		</div>
 	);
 }
